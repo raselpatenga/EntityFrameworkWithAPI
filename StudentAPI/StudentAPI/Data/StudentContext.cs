3c3318c4327f35bdbf30a4577ef1e24841f451b5
raselpatenga <63701418+raselpatenga@users.noreply.github.com>
2020-04-15 12:33:34 +0600

    Add files via upload

---
+++ StudentAPI/StudentAPI/Data/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Data
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ StudentAPI/StudentAPI/Migrations/20200415055215_initial-data.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentAPI.Migrations
+{
+    public partial class initialdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StudentName = table.Column<string>(nullable: true),
+                    StudentClass = table.Column<string>(nullable: true),
+                    StudentRoll = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
